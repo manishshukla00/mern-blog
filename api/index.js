62ddf86167c0a710be0c2f177f79378dbee695fe
rj00001111 <132373386+rj00001111@users.noreply.github.com>
2024-01-27 17:04:56 +0530

    add aniddleware and afunction to handle error

--- api/index.js
+++ api/index.js
@@ -26,3 +26,13 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((error, req, res, next) => {
+  const statusCode = error.statusCode || 500;
+  const message = error.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
