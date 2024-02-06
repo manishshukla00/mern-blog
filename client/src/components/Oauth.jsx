f33928df8d432ee7f04cd96dd0cbdaab5ef2b197
rj00001111 <132373386+rj00001111@users.noreply.github.com>
2024-02-06 20:06:32 +0530

    Aouth route created for google

--- client/src/components/Oauth.jsx
+++ client/src/components/Oauth.jsx
@@ -3,16 +3,43 @@ import { Button } from "flowbite-react";
 import { AiFillGoogleCircle } from "react-icons/ai";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const Oauth = () => {
   const auth = getAuth(app);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const handleGoogleClick = async () => {
     const provider = new GoogleAuthProvider();
     provider.setCustomParameters({ prompt: "select_account" });
     try {
       const resultsFromGoogle = await signInWithPopup(auth, provider);
+
+      const response = await axios.post(
+        "/api/auth/google",
+        {
+          name: resultsFromGoogle.user.displayName,
+          email: resultsFromGoogle.user.email,
+          googlePhotoUrl: resultsFromGoogle.user.photoURL,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await res.json();
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate("/");
+      }
     } catch (error) {
-      console.log(error);
+      // Handle errors
+      console.error("Error during POST request:", error);
     }
   };
   return (
